6b9455c81c8839dcf2164e615247a01739fa598d
yuyunfrancis <francisberi@yahoo.com>
2021-11-10 01:21:34 +0100

    navbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
 const App = () => {
   return (
     <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
 
+      </main>
+
+      <Footer />
     </>
   );
 }
